b3df0d3093a66b4b5cd112bad5e11ab08b2cee71
kinJen39 <121322132+kinJen39@users.noreply.github.com>
2023-05-26 16:35:43 -0600

    Create rectangle.groovy

---
+++ src/rectangle.groovy
@@ -0,0 +1,20 @@
+class Rectangle {
+    double length
+    double width
+    
+    // Constructor
+    Rectangle(double length, double width) {
+        this.length = length
+        this.width = width
+    }
+    
+    // Method to calculate the area
+    double calculateArea() {
+        return length * width
+    }
+    
+    // Method to calculate the perimeter
+    double calculatePerimeter() {
+        return 2 * (length + width)
+    }
+}

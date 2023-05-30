9c5bbd15d7d09c2481c49135a3ddf25d91d387be
kinJen39 <121322132+kinJen39@users.noreply.github.com>
2023-05-30 10:19:34 -0600

    Update person.groovy

--- src/person.groovy
+++ src/person.groovy
@@ -1,9 +1,54 @@
-// Comment
+/**
+ * Represents a person with a name and age.
+ */
 class Person {
     String name
     int age
     
-    void sayHello() {
-        println "Hello, my name is ${name} and I am ${age} years old."
+    /**
+     * Constructs a new Person object with the specified name and age.
+     * 
+     * @param name The name of the person.
+     * @param age  The age of the person.
+     */
+    Person(String name, int age) {
+        this.name = name
+        this.age = age
+    }
+    
+    /**
+     * Returns the name of the person.
+     *
+     * @return The name of the person.
+     */
+    String getName() {
+        return name
+    }
+    
+    /**
+     * Sets the name of the person.
+     *
+     * @param name The name of the person.
+     */
+    void setName(String name) {
+        this.name = name
+    }
+    
+    /**
+     * Returns the age of the person.
+     *
+     * @return The age of the person.
+     */
+    int getAge() {
+        return age
+    }
+    
+    /**
+     * Sets the age of the person.
+     *
+     * @param age The age of the person.
+     */
+    void setAge(int age) {
+        this.age = age
     }
 }

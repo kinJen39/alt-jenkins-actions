30435e4b1f3da096f484b840909afac5f2945544
kinJen39 <121322132+kinJen39@users.noreply.github.com>
2023-06-12 09:56:41 -0600

    Update calculator.groovy

--- src/calculator.groovy
+++ src/calculator.groovy
@@ -1,3 +1,7 @@
+/**
+ * Class that does not do anything, but is here to be a Java class run through
+ * groovydoc.
+ */
 class Calculator {
     // Method to add two numbers
     int add(int a, int b) {

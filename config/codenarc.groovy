0c3401266c7bdd8d6971abd06529aecea5eab6d0
kinJen39 <121322132+kinJen39@users.noreply.github.com>
2023-06-26 14:10:54 -0600

    Update codenarc.groovy

--- config/codenarc.groovy
+++ config/codenarc.groovy
@@ -1,28 +1,16 @@
 ruleset {
     description 'CodeNarc Project RuleSet'
 
-    ruleset('rulesets/basic.xml') {
-        include 'EmptyMethod'
-    }
-    ruleset('rulesets/convention.xml'){
-        include 'ImplicitReturnStatement'
-        include 'PublicMethodsBeforeNonPublicMethods'
-    }
-    ruleset('rulesets/exceptions.xml') {
-        include 'MissingNewInThrowStatement'
-    }
-    ruleset('rulesets/formatting.xml') {
-        LineLength {
+    EmptyMethod
+    ImplicitReturnStatement
+    PublicMethodsBeforeNonPublicMethods
+    MissingNewInThrowStatement
+    LineLength {
             length = 200
-        }
-        SpaceAroundMapEntryColon {
+    }
+    SpaceAroundMapEntryColon {
             characterAfterColonRegex = /\s/
-        }
     }
-    ruleset('rulesets/unnecessary.xml') {
-        include 'UnnecessaryElseStatement'
-    }
-    ruleset('rulesets/unused.xml') {
-        include 'UnusedMethodParameter'
-    }
+    UnnecessaryElseStatement
+    UnusedMethodParameter
 }
